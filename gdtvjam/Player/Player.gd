f37d26c467d6f466713f7437332dce4ce02ed13d
Vhal Ragnarok <bilbobaggins30@protonmail.com>
2025-05-16 20:32:23 -0500

    Push Block
    Push Block for the puzzle part. Player pushes block too slow, but apply_central_force() also can cause the block to go sliding if not careful. We can tune this later, for now it works I suppose.

--- gdtvjam/Player/Player.gd
+++ gdtvjam/Player/Player.gd
@@ -2,3 +2,19 @@ extends CharacterBody2D
 class_name player
 
 @export var movement_speed: float = 100
+@export var push_strength: float = 300
+
+func _physics_process(_delta: float) -> void:
+	check_collision()
+
+func check_collision() -> void:
+	var collision: KinematicCollision2D = get_last_slide_collision()
+
+	if (collision):
+		var collider_node = collision.get_collider()
+		if (collider_node.is_in_group("block")):
+			push_pushable(collider_node, collision)
+
+
+func push_pushable(pushable: RigidBody2D, collision: KinematicCollision2D):
+	pushable.apply_central_force(-collision.get_normal() * push_strength)

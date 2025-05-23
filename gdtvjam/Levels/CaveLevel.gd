d0ffba19d39c36f296a70b4046c0f2c4f862781f
Vhal Ragnarok <bilbobaggins30@protonmail.com>
2025-05-23 17:26:54 -0500

    Level Resetting tech.
    Okay @DoomBoy911, no more fucking features. No more, this is full freeze, full stop for Gameplay if it doesn't involve Story -> Transitioniong between level s+ actual bugs.
    
    This implements your "return to the size you entered" the level at...

---
+++ gdtvjam/Levels/CaveLevel.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+func _ready() -> void:
+    if (GameManager.level_first_load):
+        GameManager.level_first_load = false
+        GameManager.player_size_when_entered = GameManager.current_player_size
+
+    else:
+        GameManager.current_player_size = GameManager.player_size_when_entered

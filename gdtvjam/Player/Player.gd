f55b1de4a67562d4fd5eedea56716cd9f6c810b6
Vhal Ragnarok <bilbobaggins30@protonmail.com>
2025-05-26 00:48:32 -0500

    Final Bugfixes
    Removed Pulling... Its broken, I never got it to work. Fixed the border turning it "walkable" but then rely on the colliders to keep us in the game.

--- gdtvjam/Player/Player.gd
+++ gdtvjam/Player/Player.gd
@@ -29,18 +29,6 @@ func check_collision() -> void:
 			move_block(block, collision)
 
 func move_block(block: Object, collision: KinematicCollision2D) -> void:
-	if (Input.is_action_just_pressed("push_pull") && !block_is_child):
-		block_is_child = true
-		block.reparent(self)
-		block.set_deferred("freeze", true)
-		movement_speed /= 2
-	elif (Input.is_action_just_pressed("push_pull") && block_is_child):
-		block_is_child = false
-		block.reparent(get_parent().get_parent())				
-		block.set_deferred("freeze", false)
-		block.linear_velocity = Vector2.ZERO
-		movement_speed *= 2
-	else:
 		push_pushable(block, collision)
 
 
